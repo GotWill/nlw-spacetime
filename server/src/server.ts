61f5d08100ff474dccc76660ff4bfcae92486b95
Willian Pereira <williansouza9687a@gmail.com>
2023-05-20 20:24:09 -0300

    aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import 'dotenv/config'
 import jwt from '@fastify/jwt'
 import { authRoutes } from './routes/auth'
+import { uploadsRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 app.register(cors, {
   origin: true,
 })
@@ -14,6 +22,8 @@ app.register(jwt, {
 })
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadsRoutes)
+
 app
   .listen({
     port: 3333,
